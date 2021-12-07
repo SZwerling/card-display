d6f4b35bf5ddba43637a17b2ebea1a35b179a59b
Slim <sazwerling@gmail.com>
2021-12-07 17:03:43 -0600

    functional delete button

--- src/CardsComponent.js
+++ src/CardsComponent.js
@@ -4,32 +4,40 @@ import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Col, Row} fr
 class Cards extends React.Component{
     constructor(props){
         super(props);
+        this.state = { nums: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
     }
 
+    // this.setState({comment: 'Hello'});
+
+    deleteCard = (info) => {
+            let filteredNums = this.state.nums.filter((num) => {
+                return num !== info
+            })
+            this.setState({ nums: filteredNums })
+            console.log(filteredNums)
+    }
     
 
     render(){
 
-        const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-        let counter = 0;
-        const cardz = num.map((numb) => 
+       
+        const cardz = this.state.nums.map((numb) => 
           
-    <Col sm="4" className="py-1">
-        <Card key={numb}>
-            <CardBody>
+    <Col key={numb} sm="4" className="py-1">
+        <Card>
+            <CardBody className="card-body">
             <CardTitle tag="h5">
-                Card title
+                {numb}
             </CardTitle>
             <CardSubtitle
                 className="mb-2 text-muted"
                 tag="h6"
             >
-                {numb}
             </CardSubtitle>
             <CardText>
                 Some quick example text to build on the card title and make up the bulk of the card's content.
             </CardText>
-            <Button>
+            <Button color="danger" onClick={() => this.deleteCard(numb)} >
                 Button
             </Button>
             </CardBody>
